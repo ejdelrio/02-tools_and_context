4c2d987df77fba3f924777eeba1acddb4414c337
Edwin Del Rio <edwinjdelrio@gmail.com>
2017-07-18 15:37:20 -0700

    Wrote code for all standalone Array methods

--- lab-eddie/lib/fp.js
+++ lab-eddie/lib/fp.js
@@ -9,30 +9,42 @@ const typeError = (objType, expectedType) => {
 };
 
 
-function map (arr, callback) {
+function map(arr, callback) {
   let argumentLength = arguments.length;
   argumentLengthError(argumentLength, 2);
   typeError(Array.isArray(arr) ? 'array' : 'not an array', 'array');
   typeError(typeof callback, 'function');
+  return Map.prototype.map.call(arr, callback);
 }
 
-function map (arr, callback) {
+function filter(arr, callback) {
   let argumentLength = arguments.length;
   argumentLengthError(argumentLength, 2);
   typeError(Array.isArray(arr) ? 'array' : 'not an array', 'array');
   typeError(typeof callback, 'function');
+  return Map.prototype.filter.call(arr, callback);
 }
 
-function map (arr, callback) {
+function reduce(arr, callback, counter=arr[0]) {
   let argumentLength = arguments.length;
-  argumentLengthError(argumentLength, 2);
+  argumentLengthError(argumentLength, 3);
   typeError(Array.isArray(arr) ? 'array' : 'not an array', 'array');
   typeError(typeof callback, 'function');
+  return Map.prototype.reduce.call(arr, callback, counter);
 }
 
-function map (arr, callback) {
+function concat(firstArray, secondArray) {
   let argumentLength = arguments.length;
   argumentLengthError(argumentLength, 2);
-  typeError(Array.isArray(arr) ? 'array' : 'not an array', 'array');
-  typeError(typeof callback, 'function');
+  typeError(Array.isArray(firstArray) ? 'array' : 'not an array', 'array');
+  typeError(Array.isArray(secondArray) ? 'array' : 'not an array', 'array');
+  Map.prototype.concat.apply(firstArray, secondArray);
+}
+
+function splice (startingIndex, amountToDelete) {
+  let argumentLength = arguments.length;
+  argumentLengthError(argumentLength, 1);
+  typeError(typeof startingIndex, 'number');
+  typeError(typeof amountToDelete, 'number');
+  Map.prototype.splice.apply(firstArray, secondArray);
 }
